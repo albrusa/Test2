f6d15010e49ba26b1c54f6ca722a3a6229d02614
albrusa <albertrusa@gmail.com>
2015-10-16 10:52:45 +0200

    ad

--- app/src/main/java/kumo/myapplication/ConversacionActivity.java
+++ app/src/main/java/kumo/myapplication/ConversacionActivity.java
@@ -20,6 +20,9 @@ public class ConversacionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversacion);
+
+       /* Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
     }
 }
 
