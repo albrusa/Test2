4939f7d81c02c9556f55803b13cb35736c950cf2
albrusa <albertrusa@gmail.com>
2015-10-27 17:37:35 +0100

    ad

--- app/src/main/java/kumo/myapplication/ConversacionActivity.java
+++ app/src/main/java/kumo/myapplication/ConversacionActivity.java
@@ -20,6 +20,12 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.j256.ormlite.android.AndroidConnectionSource;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -49,14 +55,91 @@ public class ConversacionActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+       try {
+
+           DatabaseHelper databaseHelper = OpenHelperManager.getHelper(this,DatabaseHelper.class);
+
+           Dao<Usuario, Integer> usuarioDao = databaseHelper.getDao();
+
+           Usuario usu = new Usuario();
+           usu.id = 2;
+           usu.Nombre = "Albert";
+           usu.Apellidos = "Rubio";
+           usu.DNI = "4123123";
+           usu.Imagen = "asdasdsad";
+
+           usuarioDao.create(usu);
+
+           Usuario usu2 = usuarioDao.queryForId(2);
+
+           Log.d("Account", usu2.Nombre);
+
+           /*String databaseUrl = "jdbc:h2:mem:account";
+           // create a connection source to our database
+           ConnectionSource connectionSource = new AndroidConnectionSource(databaseUrl);
+           accountDao = DaoManager.createDao(connectionSource, Usuario.class);
+
+           TableUtils.createTableIfNotExists(connectionSource, Usuario.class);
+
+           Usuario usu = new Usuario();
+           usu.id = "123";
+           usu.Nombre = "Albert";
+
+           accountDao.create(usu);
+
+           Usuario usu2 = accountDao.queryForId("123");
+
+           Log.d("Account", usu2.Nombre);
+
+           connectionSource.close();*/
+
+
+       }catch(Exception x){
+
+           Log.d("objeto",x.getMessage());
+       }
+
+
         HashMap<String, String> params = new HashMap<String, String>();
-        //params.put("token", "AbCdEfGh123456");
 
         final JsonObject jsonObject = new JsonObject();
+
+
+        //Sense parametres
+        GsonRequest<Usuario[]> getPersons =
+                new GsonRequest<Usuario[]>(Request.Method.POST,"https://www.kmed.es/Android_API/Home/Usuario", Usuario[].class,params,jsonObject,
+
+                        new Response.Listener<Usuario[]>() {
+                            @Override
+                            public void onResponse(Usuario[] response) {
+                                List<Usuario> persons = Arrays.asList(response);
+
+                                final AdapterUsuarioListado adaptador = new AdapterUsuarioListado(persons);
+
+                                recView.setAdapter(adaptador);
+
+                                recView.setLayoutManager(
+                                        new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+                            }
+
+                        }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("objeto",error.getMessage());
+                        // TODO deal with error
+                    }
+                });
+
+        HttpCola.getInstance(this).addToRequestQueue(getPersons);
+
+        //Amb parametres
+        /*
+
+         final JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("_codigo_usuario", "albert");
         jsonObject.addProperty("_clave", "rubio");
-
-
 
         GsonRequest<Usuario[]> getPersons =
                 new GsonRequest<Usuario[]>(Request.Method.POST,"http://localhost:32766/Usuarios/autentificar_4", Usuario[].class,params,jsonObject,
@@ -73,7 +156,7 @@ public class ConversacionActivity extends AppCompatActivity {
                                 recView.setLayoutManager(
                                         new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
 
-                                /*FeedReaderDbHelper usdbh = new FeedReaderDbHelper(getApplicationContext(), "myapplication.db", null, 1);
+                                FeedReaderDbHelper usdbh = new FeedReaderDbHelper(getApplicationContext(), "myapplication.db", null, 1);
 
                                 SQLiteDatabase db = usdbh.getWritableDatabase();
 
@@ -83,7 +166,8 @@ public class ConversacionActivity extends AppCompatActivity {
 
                                     db.execSQL("INSERT INTO Cache (id, texto) " + "VALUES (" + 1 + ", '" + json +"')");
 
-                                }*/
+
+                                }
                             }
 
                         }, new Response.ErrorListener() {
@@ -96,7 +180,7 @@ public class ConversacionActivity extends AppCompatActivity {
                 });
 
         HttpCola.getInstance(this).addToRequestQueue(getPersons);
-
+        */
     }
 }
 
--- app/src/main/java/kumo/myapplication/Usuario.java
+++ app/src/main/java/kumo/myapplication/Usuario.java
@@ -1,13 +1,28 @@
 package kumo.myapplication;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Created by dev_2 on 16/10/2015.
  */
+
+@DatabaseTable(tableName = "Usuario")
 public class Usuario {
 
+    @DatabaseField(id = true)
+    Integer id;
+
+    @DatabaseField
     String Nombre;
+
+    @DatabaseField
     String Apellidos;
+
+    @DatabaseField
     String DNI;
+
+    @DatabaseField
     String Imagen;
 
     public Usuario(){
